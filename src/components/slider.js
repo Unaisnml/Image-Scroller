843264dd9ad7ccea8b73046c8d4e0318ee6e5aea
Unaisnml01~ <unaisnml@gmail.com>
2023-09-18 22:19:48 +0530

    image link update

--- src/components/slider.js
+++ src/components/slider.js
@@ -21,7 +21,7 @@ const Slider = () => {
       url: "https://images.pexels.com/photos/243206/pexels-photo-243206.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     },
     {
-      url: "Images/six.jpg",
+      url: "https://www.pexels.com/photo/selective-focus-photography-of-orange-petaled-flowers-906154/",
     },
     {
       url: "https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2671&q=80",
